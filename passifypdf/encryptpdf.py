80f1762406992bc97df0014cdc732ebce43abfc0
xaviermontane <xaviermontane3@gmail.com>
2024-10-18 11:58:55 -0400

    feat: main parser function for CLI functionality

--- passifypdf/encryptpdf.py
+++ passifypdf/encryptpdf.py
@@ -1,7 +1,7 @@
+from cli import get_parser
 from pypdf import PdfReader, PdfWriter
 
 def encrypt_pdf(input_pdf, output_pdf, password):
-
     reader = PdfReader(input_pdf)
     writer = PdfWriter()
 
@@ -16,10 +16,11 @@ def encrypt_pdf(input_pdf, output_pdf, password):
     with open(output_pdf, "wb") as f:
         writer.write(f)
 
-if __name__ == "__main__":
-    input_pdf = "Sample_PDF.pdf"   # <--- Replace with the path to your input PDF
-    output_pdf = "Sample_PDF_protected.pdf"  # <--- Replace with the output file name
-    password = "pass111"   # <--- Replace with your own choice of password
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+    encrypt_pdf(args.input, args.output, args.passwd)
+    print(f"Congratulations!\nPDF file encrypted successfully and saved as '{args.output}'")
 
-    encrypt_pdf(input_pdf, output_pdf, password)
-    print(f"Congratulations!\nPDF file encrypted successfully and saved as '{output_pdf}'")
+if __name__ == "__main__":
+    main()
